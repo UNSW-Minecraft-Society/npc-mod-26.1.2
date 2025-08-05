4c5affec64afc7e561aece4a152dd54665e2b45e
Veveddo <veveddo@macbook>
2025-08-06 03:05:52 +1000

    rename to PlanetGame

--- src/client/java/mcsoc/planetgame/PlanetgameClient.java
+++ src/client/java/mcsoc/planetgame/PlanetgameClient.java
@@ -1,8 +1,14 @@
 package mcsoc.planetgame;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.fabricmc.api.ClientModInitializer;
 
-public class PlanetgameClient implements ClientModInitializer {
+public class PlanetGameClient implements ClientModInitializer {
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(PlanetGame.MOD_ID);
+
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
