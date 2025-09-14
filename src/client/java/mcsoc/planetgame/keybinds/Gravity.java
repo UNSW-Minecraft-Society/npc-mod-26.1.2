35d16266dc9780567deee034eab7454010ac9efa
Veveddo <veveddo@macbook>
2025-09-15 00:34:42 +1000

    add keybind for gravity

---
+++ src/client/java/mcsoc/planetgame/keybinds/Gravity.java
@@ -0,0 +1,37 @@
+package mcsoc.planetgame.keybinds;
+
+import org.lwjgl.glfw.GLFW;
+
+import mcsoc.planetgame.networking.TriggerAbility1C2SPayload;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
+
+public class Gravity {
+    private static KeyBinding grav_keybind;
+
+    public static void registerBindings() {
+        grav_keybind = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "mcsoc.planetgame.grav_flip_keybind", // The translation key of the keybinding's name
+            InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+            GLFW.GLFW_KEY_R, // The keycode of the key
+            "mcsoc.planetgame.keybinds_category" // The translation key of the keybinding's category.
+        ));
+    }
+
+    public static void registerBindEvents() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (grav_keybind.wasPressed()) {
+                client.player.sendMessage(Text.literal("Key 1 was pressed!"), false);
+                ClientPlayNetworking.send(new TriggerAbility1C2SPayload());
+            }
+        });
+    }
+
+    public static void Register() {
+        
+    }
+}
