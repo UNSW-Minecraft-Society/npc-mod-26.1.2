5f7dd2f0ea22f1f4dac0571dbc0629735104b03e
Vedvod <68318644+Vedvod@users.noreply.github.com>
2026-01-25 12:48:08 +1100

    add movement parse error

--- src/main/java/mcsoc/npcmod/datatypes/npcs/MovementInstruction.java
+++ src/main/java/mcsoc/npcmod/datatypes/npcs/MovementInstruction.java
@@ -14,17 +14,31 @@ import mcsoc.npcmod.util.Instruction;
 
 
 public sealed interface MovementInstruction extends Instruction {
+    
     static final String TYPE_KEY = "type";
+    static final String ERROR_TYPE_NAME = "parse_error";
     static final String WALK_TYPE_NAME = "walk";
     static final String SWIM_TYPE_NAME = "swim";
     static final String TURN_TYPE_NAME = "turn";
     static final String STOP_TYPE_NAME = "stop";
     static final String JUMP_TYPE_NAME = "jump";
+
     static final String TICKS_PROPERTY_KEY = "ticks";
     static final String SPEED_PROPERTY_KEY = "speed";
     static final String POSITION_PROPERTY_KEY = "position";
     static final String PROPERTIES_KEY = "properties";
 
+    record ParseError() implements MovementInstruction {
+        @Override
+        public JsonObject toJson() {
+            JsonObject json = new JsonObject();
+            json.addProperty(TYPE_KEY, ERROR_TYPE_NAME);
+            JsonObject properties_json = new JsonObject();
+            json.add(PROPERTIES_KEY, properties_json);
+
+            return json;
+        }
+    }
     record Walk(PositionData pos, double speed) implements MovementInstruction {
         @Override
         public JsonObject toJson() {
@@ -95,28 +109,32 @@ public sealed interface MovementInstruction extends Instruction {
     public static MovementInstruction fromJson(JsonObject json) throws JsonParseException {
         String type_name = json.get(TYPE_KEY).getAsString();
         JsonObject properties = json.get(PROPERTIES_KEY).getAsJsonObject();
-        return (
-            switch (type_name) {
-                case WALK_TYPE_NAME -> new MovementInstruction.Walk(
-                    PositionData.fromJson(properties.get(POSITION_PROPERTY_KEY).getAsJsonObject()), 
-                    properties.get(SPEED_PROPERTY_KEY).getAsDouble()
-                );
-                case SWIM_TYPE_NAME -> new MovementInstruction.Swim(
-                    PositionData.fromJson(properties.get(POSITION_PROPERTY_KEY).getAsJsonObject()), 
-                    properties.get(SPEED_PROPERTY_KEY).getAsDouble()
-                );
-                case TURN_TYPE_NAME -> new MovementInstruction.Turn(
-                    properties.get(TICKS_PROPERTY_KEY).getAsInt(), 
-                    properties.get("degrees").getAsFloat()
-                );
-                case STOP_TYPE_NAME -> new MovementInstruction.Stop(
-                    properties.get(TICKS_PROPERTY_KEY).getAsInt()
-                );
-                case JUMP_TYPE_NAME -> new MovementInstruction.Jump();
-
-                default -> throw new JsonParseException(String.format("\"%s\" is not valid for MovementInstruction type name", type_name ));
-            }
-        );
+        try {
+            return (
+                switch (type_name) {
+                    case WALK_TYPE_NAME -> new MovementInstruction.Walk(
+                        PositionData.fromJson(properties.get(POSITION_PROPERTY_KEY).getAsJsonObject()), 
+                        properties.get(SPEED_PROPERTY_KEY).getAsDouble()
+                    );
+                    case SWIM_TYPE_NAME -> new MovementInstruction.Swim(
+                        PositionData.fromJson(properties.get(POSITION_PROPERTY_KEY).getAsJsonObject()), 
+                        properties.get(SPEED_PROPERTY_KEY).getAsDouble()
+                    );
+                    case TURN_TYPE_NAME -> new MovementInstruction.Turn(
+                        properties.get(TICKS_PROPERTY_KEY).getAsInt(), 
+                        properties.get("degrees").getAsFloat()
+                    );
+                    case STOP_TYPE_NAME -> new MovementInstruction.Stop(
+                        properties.get(TICKS_PROPERTY_KEY).getAsInt()
+                    );
+                    case JUMP_TYPE_NAME -> new MovementInstruction.Jump();
+                    case ERROR_TYPE_NAME -> new MovementInstruction.ParseError();
+                    default -> throw new JsonParseException(String.format("\"%s\" is not valid for MovementInstruction type name", type_name ));
+                }
+            );
+        } catch (Exception e) {
+            return new MovementInstruction.ParseError();
+        }
     }
 
     public static class JsonSerialiser implements JsonSerializer<MovementInstruction>, JsonDeserializer<MovementInstruction> {
