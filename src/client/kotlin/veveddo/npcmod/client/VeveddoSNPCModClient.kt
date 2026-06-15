54281a492bf3eff3d176bf4a5bcf4514d866fbb6
Veveddo <veveddo@macbook>
2026-06-15 12:20:22 +1000

    retrieve build and config files

---
+++ src/client/kotlin/veveddo/npcmod/client/VeveddoSNPCModClient.kt
@@ -0,0 +1,9 @@
+package veveddo.npcmod.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object VeveddoSNPCModClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
